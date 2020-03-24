b2f73291264232ba81d749f58b00cc0870d55be5
Jamie Cansdale <jcansdale@github.com>
2020-03-24 00:51:29 +0000

    Set home repo

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-git clone --bare . $HOME/.cfg
+git clone --bare https://github.com/jcansdale/dotfiles-test $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
